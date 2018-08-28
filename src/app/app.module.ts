f5b9762136c63bd6c9f305093447da0a3f3ade68
Daniel Chinchilla <daniel.chinchilla@molekule.com>
2018-08-27 22:34:54 -0600

    Modificaciones Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { APP_ROUTES } from './app.routes';
 import { RegisterComponent } from './login/register.component';
 
@@ -18,18 +12,11 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    PagesComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    DashboardComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
